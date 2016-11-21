aa22a493c42c6a27523520461018d589bbd38439
teuzinPereira <matheuscostapereira.puc@gmail.com>
2016-11-21 14:28:07 -0200

    Updates
    Funções de tratamento de AJAX

--- usefull.js
+++ usefull.js
@@ -20,6 +20,41 @@ $(function () {
         }
     });
       
+      $.fn.serializeObject = function ()
+    {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+    function deserializeObject(form, data) {
+        $.each(data, function (name, val) {
+            var $el = $('#' + form + ' [name="' + name + '"]'),
+                    type = $el.attr('type');
+
+            switch (type) {
+                case 'checkbox':
+                    $el.attr('checked', 'checked');
+                    break;
+                case 'radio':
+                    $el.filter('[value="' + val + '"]').attr('checked', 'checked');
+                    break;
+                default:
+                    $el.val(val);
+            }
+        });
+    }
+      
       jQuery(".mask-telefone")
         .mask("(99) 9999-9999?9")
         .focusout(function (event) {
